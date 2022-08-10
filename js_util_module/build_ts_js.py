f6f572cad5c03bcc89b84ee09f87bc2a2993914a
jorzion <jorzion@protonmail.com>
2022-08-10 15:09:31 +0800

    Correct spelling mistakes in English words
    Signed-off-by: jorzion <jorzion@protonmail.com>

--- js_util_module/build_ts_js.py
+++ js_util_module/build_ts_js.py
@@ -46,13 +46,13 @@ if __name__ == '__main__':
     NODE_PATH = '../../../../prebuilts/build-tools/common/nodejs/\
 node-v12.18.4-linux-x64/bin/node'
     if not os.path.exists(NODE_PATH):
-        raise Exception('NO souch file or directory')
+        raise Exception('error:NO such file or directory')
     TSC_PATH = '../../../../arkcompiler/ets_frontend/ts2panda/node_modules/\
 typescript/bin/tsc'
     CMD_INST = [NODE_PATH, TSC_PATH]
     run_command(CMD_INST)
     if not os.path.exists(INPUT_ARGUMENTS.out_file):
-        raise Exception('error:NO souch file or directory')
+        raise Exception('error:NO such file or directory')
     CMD_INST = shutil.copy(INPUT_ARGUMENTS.out_file, INPUT_ARGUMENTS.dst_file)
 
     CMD_INST = shutil.rmtree('./out')
