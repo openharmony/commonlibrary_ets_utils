2cf60f191310cac9fd4569a829dc9062d0a46d78
wwx1247055 <wangben23@h-partners.com>
2023-04-18 16:56:11 +0800

    Description: Modify Format
    issues:https://gitee.com/openharmony/commonlibrary_ets_utils/issues/I6WQVS
    
    Signed-off-by: wwx1247055 <wangben23@huawei.com>

--- js_api_module/uri/src/js_uri.ts
+++ js_api_module/uri/src/js_uri.ts
@@ -12,159 +12,157 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-interface NativeUri{
-    new(input : string) : NativeUri;
-    normalize() : string;
-    equals(other : NativeUri) : boolean;
-    equalsTo(other : NativeUri) : boolean;
-    checkIsAbsolute() : boolean;
-    toString() : string;
-    scheme : string;
-    authority : string;
-    ssp : string;
-    userInfo : string;
-    host : string;
-    port : string;
-    path : string;
-    query : string;
-    fragment : string;
-    isFailed : string;
+interface NativeUri {
+  new(input: string): NativeUri;
+  normalize(): string;
+  equals(other: NativeUri): boolean;
+  equalsTo(other: NativeUri): boolean;
+  checkIsAbsolute(): boolean;
+  toString(): string;
+  scheme: string;
+  authority: string;
+  ssp: string;
+  userInfo: string;
+  host: string;
+  port: string;
+  path: string;
+  query: string;
+  fragment: string;
+  isFailed: string;
 }
-interface UriInterface{
-    Uri : NativeUri;
+interface UriInterface {
+  Uri: NativeUri;
 }
-declare function requireInternal(s : string) : UriInterface;
+declare function requireInternal(s: string): UriInterface;
 const uri = requireInternal('uri');
 
 const TypeErrorCodeId = 401;
 const SyntaxErrorCodeId = 10200002;
 
 class BusinessError extends Error {
-    code : number;
-    constructor(msg:string) {
-        super(msg)
-        this.name = 'BusinessError';
-        this.code = TypeErrorCodeId;
-    }
+  code: number;
+  constructor(msg: string) {
+    super(msg)
+    this.name = 'BusinessError';
+    this.code = TypeErrorCodeId;
+  }
 }
 
 class URI {
-    uricalss : NativeUri
-    constructor(input : string) {
-        if (typeof input !== 'string' || input.length === 0) {
-            let err = new BusinessError(`Parameter error.The type of ${input} must be string`);
-            throw err;
-        }
-        this.uricalss = new uri.Uri(input);
-        let errStr : string = this.uricalss.isFailed;
-        if (errStr.length !== 0) {
-            let err = new BusinessError('Syntax Error. Invalid Uri string');
-            err.code = SyntaxErrorCodeId;
-            throw err;
-        }
+  uricalss: NativeUri
+  constructor(input: string) {
+    if (typeof input !== 'string' || input.length === 0) {
+      throw new BusinessError(`Parameter error.The type of ${input} must be string`);
     }
-    toString() {
-        return toAscllString(this.uricalss.toString());
+    this.uricalss = new uri.Uri(input);
+    let errStr: string = this.uricalss.isFailed;
+    if (errStr.length !== 0) {
+      let err : BusinessError = new BusinessError('Syntax Error. Invalid Uri string');
+      err.code = SyntaxErrorCodeId;
+      throw err;
     }
+  }
+  toString(): string {
+    return toAscllString(this.uricalss.toString());
+  }
 
-    equals(other : URI) {
-        return this.uricalss.equals(other.uricalss);
-    }
+  equals(other: URI): boolean {
+    return this.uricalss.equals(other.uricalss);
+  }
 
-    equalsTo(other : URI) {
-        if (other instanceof URI) {
-            return this.uricalss.equals(other.uricalss);
-        }
-        let err = new BusinessError(`Parameter error.The type of ${other} must be URI`);
-        throw err;
+  equalsTo(other: URI): boolean {
+    if (other instanceof URI) {
+      return this.uricalss.equals(other.uricalss);
     }
+    throw new BusinessError(`Parameter error.The type of ${other} must be URI`);
+  }
 
-    checkIsAbsolute() {
-        return this.uricalss.checkIsAbsolute();
-    }
+  checkIsAbsolute(): boolean {
+    return this.uricalss.checkIsAbsolute();
+  }
 
-    normalize() {
-        let uriStr : string = this.uricalss.normalize();
-        return createNewUri(uriStr);
-    }
+  normalize(): URI {
+    let uriStr: string = this.uricalss.normalize();
+    return createNewUri(uriStr);
+  }
 
-    get scheme() {
-        return this.uricalss.scheme;
-    }
+  get scheme(): string {
+    return this.uricalss.scheme;
+  }
 
-    get authority() {
-        let thisAuthority : string = this.uricalss.authority;
-        if (thisAuthority.indexOf('[') !== -1) {
-            let arr : string[] = thisAuthority.split('[');
-            let brr : string[] = arr[1].split(']');
-            arr[1] = '[' + brr[0] + ']';
-            arr[2] = brr[1];
-            arr[0] = decodeURIComponent(arr[0]);
-            arr[2] = decodeURIComponent(arr[2]);
-            return arr.join('');
-        } else {
-            return decodeURIComponent(thisAuthority);
-        }
-    }
-
-    get ssp() {
-        let thisSsp : string = this.uricalss.ssp;
-        if (thisSsp.indexOf('[') !== -1) {
-            let arr : string[] = thisSsp.split('[');
-            let brr : string[] = arr[1].split(']');
-            arr[1] = '[' + brr[0] + ']';
-            arr[2] = brr[1];
-            arr[0] = decodeURIComponent(arr[0]);
-            arr[2] = decodeURIComponent(arr[2]);
-            return arr.join('');
-        } else {
-            return decodeURIComponent(thisSsp);
-        }
-    }
-
-    get userInfo() {
-        return decodeURIComponent(this.uricalss.userInfo);
-    }
-
-    get host() {
-        return this.uricalss.host;
-    }
-
-    get port() {
-        return this.uricalss.port;
-    }
-
-    get path() {
-        return decodeURIComponent(this.uricalss.path);
-    }
-
-    get query() {
-        return decodeURIComponent(this.uricalss.query);
-    }
-
-    get fragment() {
-        return decodeURIComponent(this.uricalss.fragment);
-    }
-}
-
-function toAscllString(uriStr : string) {
-    if (uriStr.indexOf('[') !== -1) {
-        let arr : string[] = uriStr.split('[');
-        let brr : string[] = arr[1].split(']');
-        arr[1] = '[' + brr[0] + ']';
-        arr[2] = brr[1];
-        arr[0] = encodeURI(arr[0]);
-        arr[2] = encodeURI(arr[2]);
-        return arr.join('');
+  get authority(): string {
+    let thisAuthority: string = this.uricalss.authority;
+    if (thisAuthority.indexOf('[') !== -1) {
+      let arr: string[] = thisAuthority.split('[');
+      let brr: string[] = arr[1].split(']');
+      arr[1] = '[' + brr[0] + ']';
+      arr[2] = brr[1];
+      arr[0] = decodeURIComponent(arr[0]);
+      arr[2] = decodeURIComponent(arr[2]);
+      return arr.join('');
     } else {
-        return encodeURI(uriStr);
+      return decodeURIComponent(thisAuthority);
     }
+  }
+
+  get ssp(): string {
+    let thisSsp: string = this.uricalss.ssp;
+    if (thisSsp.indexOf('[') !== -1) {
+      let arr: string[] = thisSsp.split('[');
+      let brr: string[] = arr[1].split(']');
+      arr[1] = '[' + brr[0] + ']';
+      arr[2] = brr[1];
+      arr[0] = decodeURIComponent(arr[0]);
+      arr[2] = decodeURIComponent(arr[2]);
+      return arr.join('');
+    } else {
+      return decodeURIComponent(thisSsp);
+    }
+  }
+
+  get userInfo(): string {
+    return decodeURIComponent(this.uricalss.userInfo);
+  }
+
+  get host(): string {
+    return this.uricalss.host;
+  }
+
+  get port(): string {
+    return this.uricalss.port;
+  }
+
+  get path(): string {
+    return decodeURIComponent(this.uricalss.path);
+  }
+
+  get query(): string {
+    return decodeURIComponent(this.uricalss.query);
+  }
+
+  get fragment(): string {
+    return decodeURIComponent(this.uricalss.fragment);
+  }
 }
 
-function createNewUri(uriStr : string) {
-    return new URI(uriStr);
+function toAscllString(uriStr: string): string {
+  if (uriStr.indexOf('[') !== -1) {
+    let arr: string[] = uriStr.split('[');
+    let brr: string[] = arr[1].split(']');
+    arr[1] = '[' + brr[0] + ']';
+    arr[2] = brr[1];
+    arr[0] = encodeURI(arr[0]);
+    arr[2] = encodeURI(arr[2]);
+    return arr.join('');
+  } else {
+    return encodeURI(uriStr);
+  }
 }
 
-export default  {
-    URI : URI
+function createNewUri(uriStr: string): URI {
+  return new URI(uriStr);
+}
+
+export default {
+  URI: URI
 }
