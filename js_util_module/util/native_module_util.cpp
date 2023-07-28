763178425f41609f387dfd60f1d9de3f6ab30116
openharmony_ci <120357966@qq.com>
2023-07-28 08:04:18 +0000

    回退 'Pull Request !617 : 跨平台gn重构'

--- js_util_module/util/native_module_util.cpp
+++ js_util_module/util/native_module_util.cpp
@@ -27,14 +27,17 @@
 
 #include "napi/native_api.h"
 #include "napi/native_node_api.h"
-#include "jni_helper.h"
+
+#ifdef ANDROID_PLATFORM
+#include "plugin_utils.h"
+#include "util_plugin_jni.h"
+#endif
 
 extern const char _binary_util_js_js_start[];
 extern const char _binary_util_js_js_end[];
 extern const char _binary_util_abc_start[];
 extern const char _binary_util_abc_end[];
 namespace OHOS::Util {
-    using namespace Commonlibrary::Platform;
     static bool IsValidValue(napi_env env, napi_value value)
     {
         napi_value undefinedRef = nullptr;
@@ -1717,11 +1720,21 @@ namespace OHOS::Util {
         .nm_get_abc_code = NAPI_util_GetABCCode,
         .nm_get_js_code = NAPI_util_GetJSCode,
     };
+
+#ifdef ANDROID_PLATFORM
+    static void UtilPluginJniRegister()
+    {
+        const char className[] = "ohos.ace.plugin.utilplugin.UtilPlugin";
+        ARKUI_X_Plugin_RegisterJavaPlugin(&Plugin::UtilPluginJni::Register, className);
+    }
+#endif
     // util module register
     extern "C"
     __attribute__((constructor)) void UtilRegisterModule()
     {
         napi_module_with_js_register(&utilModule);
-        UtilPluginJniRegister();
+#ifdef ANDROID_PLATFORM
+        ARKUI_X_Plugin_RunAsyncTask(&UtilPluginJniRegister, ARKUI_X_PLUGIN_PLATFORM_THREAD);
+#endif
     }
 }
