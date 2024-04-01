af427e8cae9bcefa271ddba7094a60270d3574ab
bi-hu <bihu@huawei.com>
2024-04-01 09:34:27 +0800

    Compilation issues for adapting to Arkui x
    Signed-off-by: bi-hu <bihu@huawei.com>
    https://gitee.com/openharmony/commonlibrary_ets_utils/issues/I9D33F

--- platform/default/util_helper.cpp
+++ platform/default/util_helper.cpp
@@ -85,4 +85,22 @@ void EncodeIntoChinese(napi_env env, napi_value src, std::string encoding, std::
     buffer = UtilPlugin::EncodeIntoChinese(input, encoding);
 }
 
+std::string UnicodeConversion(std::string encoding, char16_t* originalBuffer, size_t inputSize)
+{
+    std::string buffer = "";
+    std::u16string tempStr16(originalBuffer, inputSize);
+    buffer = std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> {}.to_bytes(tempStr16);
+    std::string resultBuffer = "";
+    resultBuffer = UtilPlugin::EncodeIntoChinese(buffer, encoding);
+    return resultBuffer;
+}
+
+int GetMaxByteSize(std::string encoding)
+{
+    int byteSize = 4; // 4:byte size
+    if (!encoding.empty()) {
+        return byteSize;
+    }
+    return 0;
+}
 } // namespace Commonlibrary::Platform
