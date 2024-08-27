23b5bd978be03250d73d731b7ee5c5479a6f6d5a
xusen <xusen15@huawei.com>
2024-08-27 20:23:08 +0800

    Add taskpool/worker ut
    Add taskpool/worker ut
    
    Issue:  https://gitee.com/openharmony/commonlibrary_ets_utils/issues/IAMQ6R
    Signed-off-by: xusen <xusen15@huawei.com>

--- js_concurrent_module/taskpool/task_runner.cpp
+++ js_concurrent_module/taskpool/task_runner.cpp
@@ -15,7 +15,9 @@
 
 #include "task_runner.h"
 
+#include "helper/concurrent_helper.h"
 #include "helper/object_helper.h"
+#include "tools/log.h"
 
 namespace Commonlibrary::Concurrent::TaskPoolModule {
 using namespace Commonlibrary::Concurrent::Common::Helper;
@@ -28,14 +30,22 @@ TaskRunner::~TaskRunner()
 
 void TaskRunner::TaskInnerRunner::Run()
 {
-    runner_->Run();
+    if (LIKELY(runner_ != nullptr)) {
+        runner_->Run();
+    } else { // LCOV_EXCL_BR_LINE
+        HILOG_FATAL("taskpool:: runner_ is null");
+    }
 }
 
 TaskRunner::TaskInnerRunner::TaskInnerRunner(const TaskRunner* runner) : runner_(runner) {}
 
 void TaskRunner::Run() const
 {
-    callback_.callback(callback_.data);
+    if (LIKELY(callback_.callback != nullptr)) {
+        callback_.callback(callback_.data);
+    } else { // LCOV_EXCL_BR_LINE
+        HILOG_FATAL("taskpool:: callback_.callback is null");
+    }
 }
 
 bool TaskRunner::Execute()
