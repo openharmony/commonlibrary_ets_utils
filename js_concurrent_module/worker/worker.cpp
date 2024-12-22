eacc6b7e836f47285e6b5b8be6f6fcbd43edc6f2
jinjiawei <jinjiawei2@huawei.com>
2024-12-22 20:40:04 +0800

    add one Policy that woker trigger gc when thread idle Issue: https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/IB911Q
    Signed-off-by: jinjiawei <jinjiawei2@huawei.com>
    Change-Id: I7c025d4aabdc0b0caebafbc44dde2cf2c8787b87

--- js_concurrent_module/worker/worker.cpp
+++ js_concurrent_module/worker/worker.cpp
@@ -29,6 +29,7 @@
 #ifdef ENABLE_QOS
 #include "qos.h"
 #endif
+#include "native_engine.h"
 
 namespace Commonlibrary::Concurrent::WorkerModule {
 using namespace OHOS::JsSysModule;
@@ -2072,6 +2073,7 @@ void Worker::WorkerOnMessageInner()
     if (IsTerminated()) {
         return;
     }
+    WorkerRunningScope workerRunningScope(workerEnv_);
     napi_status status;
     napi_handle_scope scope = nullptr;
     status = napi_open_handle_scope(workerEnv_, &scope);
